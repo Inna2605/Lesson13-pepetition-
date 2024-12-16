5f8c88ea0cf7bdcb4df79376071ff0e5e2544438
Inna <prizrak1609@gmail.com>
2024-12-16 19:19:53 +0200

    Add project files.

---
+++ Zadacha1.cpp
@@ -0,0 +1,74 @@
+/*1. Дано два масиви: А[N] і B[M] (M і N вводяться з клавіатури).
+Необхідно створити третій масив мінімально можливого розміру,
+у якому потрібно зібрати елементи масиву A, які не включаються до масиву B, без повторень.*/
+
+#include<iostream>
+#include "Zagalne.h"
+#include "Zadacha1.h"
+
+int* Ne_vklucheni_elementu_A_v_B(int &P,int &N, int &M, int A[], int B[]);
+void Kopiuvanna_masuvy(int P, int C[], int D[]);
+
+void Elementu_A_ne_vklucheni_v_B()
+{
+	int N, M, P;
+	std::cout << "Vvedite rozmiru masuviv A i B: ";
+	Vvod(N);
+	Vvod(M);
+	int* A = new int[N];
+	int* B = new int[M];
+	Masuv(N, A);
+	std::cout << "Masuv A:\n";
+	Show_masuv(N, A);
+	Masuv(M, B);
+	std::cout << "Masuv B:\n";
+	Show_masuv(M, B);
+	int* C  = Ne_vklucheni_elementu_A_v_B(P, N, M, A, B);
+	std::cout << "Masuv C:\n";
+	Show_masuv(P, C);
+}
+
+int* Ne_vklucheni_elementu_A_v_B(int& P, int& N, int& M, int A[], int B[])
+{
+	P = 0;
+	int r;
+	int* C = nullptr;
+	for (int i = 0; i < N; i++) {
+		r = 0;
+		for (int j = 0; j < M; j++) {
+			if (A[i] == B[j]) {
+				r++;
+				break;
+			}
+		}
+		if (r == 0 && P == 0) {
+			P++;
+			C = new int[P];
+			C[P - 1] = A[i];
+		}
+		else if (r == 0 && P != 0) {
+			for (int h = 0; h < P; h++) {
+				if (C[h] == A[i]) {
+					r++;
+		
+				}
+			}
+			if (r == 0) {
+				P++;
+				int* D = new int[P];
+				Kopiuvanna_masuvy(P - 1, C, D);
+				D[P - 1] = A[i];
+				delete[]C;
+				C = D;
+			}
+		}
+	}
+	return C;
+}
+
+void Kopiuvanna_masuvy(int P, int C[], int D[])
+{
+	for (int i = 0; i < P; i++) {
+		D[i] = C[i];
+	}
+}
---
+++ Zagalne.cpp
@@ -0,0 +1,52 @@
+/*1. Дано два масиви: А[M] і B[N] (M і N вводяться з клавіатури).
+Необхідно створити третій масив мінімально можливого розміру,
+у якому потрібно зібрати елементи масиву A, які не включаються до масиву B, без повторень.
+2. Дано два масиви: А[M] і B[N] (M і N вводяться з клавіатури).
+Необхідно створити третій масив мінімально можливого розміру,
+у якому потрібно зібрати елементи масивів A і B, які не є загальними для них, без повторень.
+3. Дано два масиви: А[M] і B[N] (M і N вводяться з клавіатури).
+Необхідно створити третій масив мінімально можливого розміру,
+у якому потрібно зібрати елементи обох масивів.
+4. Дано масив: А[M] (M вводиться з клавіатури).
+Необхідно видалити з масиву парні або непарні значення.
+Користувач вводить дані в масив, а також за допомогою меню вирішує, що потрібно видалити.*/
+
+#include "Zagalne.h"
+#include<iostream>
+
+void Vubir_zadachi()
+{
+	std::cout << "Oberite zadachy:\n"
+		<< "1. Elementu A ne vklucheni v B\n"
+		<< "2. Elementu A i B ne zagalni dla nux\n"
+		<< "3. Masuv z elementamu A i B\n"
+		<< "4. Vudalenna elementiv z masuva\n"
+		<< "5. Vuxid\n";
+}
+
+void Swich_zadach(int &a)
+{
+	switch (a) {
+	case 1:Elementu_A_ne_vklucheni_v_B(); break;
+	case 2:/*Elementu_A_i_B_ne_zagalni_dla_nux();*/break;
+	case 3:/*Masuv_z_elementamu_A_i_B();*/break;
+	case 4:/*Vudalenna_elementiv_z_masuva();*/break;
+	case 5:break;
+	}
+}
+
+void Masuv(int& a, int A[])
+{
+	for (int i = 0; i < a; i++) {
+		A[i] = rand() % 20 - 10;
+	}
+}
+
+void Show_masuv(int& a, int A[])
+{
+	for (int i = 0; i < a; i++) {
+		Show(A[i]);
+		Show(' ');
+	}
+	Show('\n');
+}
